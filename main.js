bb5d7c7eeec1e1f0b5698110bdfc53f2383d8872
Pcamellon <pedropcamellon@gmail.com>
2022-10-14 12:33:38 -0400

    Add styles

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ import './style.css'
 
 // document.querySelector('#app').innerHTML = `
 //   <div>
-    
+
 //   </div>
 // `
 
 // HTML elements
 const mainWrapper = document.getElementById("main-wrapper"),
-      main = document.getElementById("main");
+  main = document.getElementById("main");
 
 // Hide sidebar on scroll down
 mainWrapper.onscroll = e => {
@@ -16,7 +16,7 @@ mainWrapper.onscroll = e => {
     main.dataset.nav = "inactive";
   }
 }
- 
+
 // Attach fn globally
 window.toggleNav = () => {
   const active = mainWrapper.scrollTop === 0 ? "active" : "fixed";
@@ -24,6 +24,3 @@ window.toggleNav = () => {
   main.dataset.nav = main.dataset.nav === active ? "inactive" : active;
 }
 
-    
-  </div>
-`

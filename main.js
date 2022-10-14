a8a7cb70cc5ca65f7b797489be3ed925638d8b34
Pcamellon <pedropcamellon@gmail.com>
2022-10-14 12:32:06 -0400

    Hide sidebar on scroll

--- main.js
+++ main.js
@@ -10,6 +10,15 @@ import './style.css'
 const mainWrapper = document.getElementById("main-wrapper"),
       main = document.getElementById("main");
 
+// Hide sidebar on scroll down
+mainWrapper.onscroll = e => {
+  if (main.dataset.nav !== "inactive") {
+    main.dataset.nav = "inactive";
+  }
+}
+ 
+}
+
     
   </div>
 `

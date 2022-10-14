0ca11473431393b78744bd298ec60953dafc7bf6
Pcamellon <pedropcamellon@gmail.com>
2022-10-14 12:29:16 -0400

    Add references to main containers in main.js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
+// document.querySelector('#app').innerHTML = `
+//   <div>
+    
+//   </div>
+// `
+
+// HTML elements
+const mainWrapper = document.getElementById("main-wrapper"),
+      main = document.getElementById("main");
+
     
   </div>
 `
